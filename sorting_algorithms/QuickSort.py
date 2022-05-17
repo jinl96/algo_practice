daaeca2781cf31a20521db8f43dd4e91a009f0dc
jinl96 <jinyangliu1996@gmail.com>
2022-05-17 10:32:41 +0800

    quicksort descending order implemented

--- sorting_algorithms/QuickSort.py
+++ sorting_algorithms/QuickSort.py
@@ -11,18 +11,50 @@ def partition(low, high, arr):
     return j
 
 
-def quick_sort(arr, low=None, high=None):
+def quick_sort(arr, low=None, high=None, dsc=False):
     if len(arr) < 2:
         return arr
     low = 0 if not isinstance(low, (int, float)) else low
-    high = 0 if not isinstance(high, (int, float)) else high
+    high = len(arr) - 1 if not isinstance(high, (int, float)) else high
     if low < high:
-        pivot = partition(low, high, arr)
-        quick_sort(arr, low, pivot)
-        quick_sort(arr, pivot + 1, high)
+        if dsc:
+            pivot = des_partition(low, high, arr)
+            quick_sort(arr, low, pivot, dsc=True)
+            quick_sort(arr, pivot + 1, high, dsc=True)
+        else:
+            pivot = partition(low, high, arr)
+            quick_sort(arr, low, pivot)
+            quick_sort(arr, pivot + 1, high)
+    return arr
+
+
+def des_partition(high, low, arr):
+    i, j = high, low
+
+    while i < j:
+        while i < j and arr[j] <= arr[high]:
+            j -= 1
+        while i < j and arr[i] >= arr[high]:
+            i += 1
+        arr[i], arr[j] = arr[j], arr[i]
+    arr[high], arr[j] = arr[j], arr[high]
+    return j
+
+
+def des_quick_sort(arr, high=None, low=None):
+    if len(arr) < 2:
+        return arr
+    high = 0 if not isinstance(high, (int, float)) else high
+    low = len(arr) - 1 if not isinstance(low, (int, float)) else low
+    if low > high:
+        pivot = des_partition(high, low, arr)
+        des_quick_sort(arr, high, pivot)
+        des_quick_sort(arr, pivot + 1, low)
     return arr
 
 
 test_arr = [5, 1, 23, 3, 1, 2, 3, 1, 123, 324, 51123, 23, 1, 1, 2, 3, 4, 5]
-test_arr = quick_sort(test_arr)
-print(test_arr)
+# test_arr = quick_sort(test_arr)
+des_arr = quick_sort(test_arr)
+# print(test_arr)
+print(des_arr)
